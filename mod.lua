f916aa2bb5e74710a0b60823f4fe21db9675273a
Florian Thiel <flth@flth.de>
2023-04-06 22:55:46 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,63 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "deutschland", "germany", "db", "waggon", "goods", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+        params = {
+			{
+				key = "tamnsfake",
+				name = _("Fake_tamns_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_tamns_desc"),
+				defaultIndex = 0,
+			},
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	
+        local hidden = {
+			["886_braun_DBAG_fake.mdl"] = true,   
+			["886_DB_fake.mdl"] = true,   
+			["886_DBAG_fake.mdl"] = true,   
+			["886x_DBAG_fake.mdl"] = true,   
+			["893_braun_DBAG_fake.mdl"] = true,   
+			["893_buehne_braun_DBAG_fake.mdl"] = true,   
+			["893_buehne_DB_fake.mdl"] = true,   
+			["893_buehne_DBAG_fake.mdl"] = true,   
+			["893_DB_fake.mdl"] = true,  
+			["893_DBAG_fake.mdl"] = true,           
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/tamns([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["tamnsfake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,103 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Tamns 886 and 893 DB",
+		["mod_desc"] = "This mod contains the Tamns 886, Tamns-x 886 and Tamns 893 four-axle freight wagons. The package contains numerous DB and DB AG liveries.",
+        
+        ["Fake_tamns_wagen"] = "Enable Tamns' Fake Versions?",
+        ["option_fake_tamns_desc"] = "Should the fake versions of the Tamns 886 and 893 be available in the game?",
+
+        -- Lackierungen
+        ["Lack_DB"] = " Paintjob: red brown Deutsche Bundesbahn (DB)",
+        ["Lack_DBAG"] = " Paintjob: red brown Deutsche Bahn AG",
+        ["Lack_vr_DBAG"] = " Paintjob: traffic red DB AG",
+        
+        -- Tamns
+        ["tamns_menu"] = "Tamns (openable goods wagons)",
+				
+        ["tamns_886_DB_name"] = "DB Tamns 886 (1986)",
+        ["tamns_886_DBAG_name"] = "DB Tamns 886 (1994)",
+        ["tamns_886_braun_DBAG_name"] = "DB Tamns 886 (1994)",
+        ["tamns_886_desc"] = "In 1986, DB had 20 Eaos 051 wagons converted by Talbot to Tamns 886 with plastic rolling roofs for the transport of weather-sensitive bulk goods.",
+		
+        ["tamns_886x_DBAG_name"] = "DB Tamns-x 886 (2005)",
+        ["tamns_886x_desc"] = "Starting in 2005, DB will convert all 20 Tamns 886 wagons for the transport of potassium chloride. The designation is changed to Tamns-x 886, the soft top, roof drive and tarpaulin storage baskets are removed and the doors are fixed.",
+		
+		
+        ["tamns_893_DB_name"] = "DB Tamns 893 (1987)",
+        ["tamns_893_DBAG_name"] = "DB Tamns 893 (1994)",
+        ["tamns_893_braun_DBAG_name"] = "DB Tamns 893 (1994)",
+        ["tamns_893_buehne_DB_name"] = "DB Tamns 893 with stage (1987)",
+        ["tamns_893_buehne_DBAG_name"] = "DB Tamns 893 with stage (1994)",
+        ["tamns_893_braun_buehne_DBAG_name"] = "DB Tamns 893 with stage (1994)",
+        ["tamns_893_desc"] = "Due to the good experiences with the conversion of the Eanos 051 to Tamns 866, the DB had 200 Eanos 052 wagons with a length of 15.7m converted to Tamns 893 in 1987. The cars get a soft top, roof drive, tarpaulin storage basket and the right door is removed.",
+		
+		--- Fake 
+        ["tamns_menu_fake"] = "Tamns (openable goods wagons) FAKE",
+		
+        ["tamns_886_DB_name_fake"] = "DB Tamns 886 (1986) FAKE",
+        ["tamns_886_DBAG_name_fake"] = "DB Tamns 886 (1994) FAKE",
+        ["tamns_886_braun_DBAG_name_fake"] = "DB Tamns 886 (1994) FAKE",
+        ["tamns_886x_DBAG_name_fake"] = "DB Tamns 886 (2005) FAKE",
+		
+        ["tamns_893_DB_name_fake"] = "DB Tamns 893 (1987) FAKE",
+        ["tamns_893_DBAG_name_fake"] = "DB Tamns 893 (1994) FAKE",
+        ["tamns_893_braun_DBAG_name_fake"] = "DB Tamns 893 (1994) FAKE",
+        ["tamns_893_buehne_DB_name_fake"] = "DB Tamns 893 with stage (1987) FAKE",
+        ["tamns_893_buehne_DBAG_name_fake"] = "DB Tamns 893 with stage (1994) FAKE",
+        ["tamns_893_braun_buehne_DBAG_name_fake"] = "DB Tamns 893 with stage (1994) FAKE",
+        
+
+	},
+	de = {
+
+		["mod_name"] = "Tamns 886 und 893 DB",
+		["mod_desc"] = "Dieser Mod enthält die vierachsigen Güterwagen des Typs Tamns 886, Tamns-x 886 und Tamns 893. Das Paket enthält zahlreichen Lackierungen der DB und DB AG.",
+        
+        ["Fake_tamns_wagen"] = "Tamns Fake-Versionen aktivieren?",
+        ["option_fake_tamns_desc"] = "Sollen die Fake-Versionen der Tamns 886 und 893 verfügbar sein im Spiel?",
+
+        -- Lackierungen
+        ["Lack_DB"] = " Lackierung: rotbraun Deutsche Bundesbahn (DB)",
+        ["Lack_DBAG"] = " Lackierung: rotbraun Deutsche Bahn AG",
+        ["Lack_vr_DBAG"] = " Lackierung: verkehrsrot DB AG",
+        
+        -- Tamns
+        ["tamns_menu"] = "Tamns (öffnungsfähige Güterwagen)",
+		
+        ["tamns_886_DB_name"] = "DB Tamns 886 (1986)",
+        ["tamns_886_DBAG_name"] = "DB Tamns 886 (1994)",
+        ["tamns_886_braun_DBAG_name"] = "DB Tamns 886 (1994)",
+        ["tamns_886_desc"] = "Im Jahr 1986 lässt die DB 20 Wagen des Typs Eaos 051 von Talbot zu Tamns 886 mit Kunststoff-Rolldächern für den Transport von witterungsempfindlichen Schüttgütern umbauen.",
+		
+        ["tamns_886x_DBAG_name"] = "DB Tamns-x 886 (2005)",
+        ["tamns_886x_desc"] = "Ab dem Jahr 2005 baut die DB alle 20 Wagen des Typs Tamns 886 um für den Transport von Kaliumchlorid. Dabei wird die Bezeichnung in Tamns-x 886 geändert, Rolldach, Dachantrieb und Planenenaufbewahrungskörbe werden abgebaut und die Türen werden festgelegt.",
+		
+		
+        ["tamns_893_DB_name"] = "DB Tamns 893 (1987)",
+        ["tamns_893_DBAG_name"] = "DB Tamns 893 (1994)",
+        ["tamns_893_braun_DBAG_name"] = "DB Tamns 893 (1994)",
+        ["tamns_893_buehne_DB_name"] = "DB Tamns 893 mit Bühne (1987)",
+        ["tamns_893_buehne_DBAG_name"] = "DB Tamns 893 mit Bühne (1994)",
+        ["tamns_893_braun_buehne_DBAG_name"] = "DB Tamns 893 mit Bühne (1994)",
+        ["tamns_893_desc"] = "Durch die guten Erfahrungen mit dem Umbau der Eaos 051 zu Tamns 866, lässt die DB im Jahr 1987 200 Wagen des Typs Eanos 052 mit eine Länge von 15,7m in Tamns 893 umbauen. Die Wagen erhalten ein Rolldach, Dachantrieb, Planenaufbewahrungskorb und es wird die jeweils Rechte Tür entfernt.",
+		
+		--- Fake 
+        ["tamns_fake_menu"] = "Tamns (öffnungsfähige Güterwagen) FAKE",
+		
+        ["tamns_886_DB_name_fake"] = "DB Tamns 886 (1986) FAKE",
+        ["tamns_886_DBAG_name_fake"] = "DB Tamns 886 (1994) FAKE",
+        ["tamns_886_braun_DBAG_name_fake"] = "DB Tamns 886 (1994) FAKE",
+        ["tamns_886x_DBAG_name_fake"] = "DB Tamns 886 (2005) FAKE",
+		
+        ["tamns_893_DB_name_fake"] = "DB Tamns 893 (1987) FAKE",
+        ["tamns_893_DBAG_name_fake"] = "DB Tamns 893 (1994) FAKE",
+        ["tamns_893_braun_DBAG_name_fake"] = "DB Tamns 893 (1994) FAKE",
+        ["tamns_893_buehne_DB_name_fake"] = "DB Tamns 893 mit Bühne (1987) FAKE",
+        ["tamns_893_buehne_DBAG_name_fake"] = "DB Tamns 893 mit Bühne (1994) FAKE",
+        ["tamns_893_braun_buehne_DBAG_name_fake"] = "DB Tamns 893 mit Bühne (1994) FAKE",
+		
+	},
+}
+end
